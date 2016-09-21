059e129ba2960adb3d8e50ef9c0fbb6394e249a5
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-09-21 13:27:28 +0100

    Write a toggle

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const midi = require('midi')
 const input = new midi.input()
 const output = new midi.output()
 const midiLaunchpad = require('midi-launchpad')
-const {red, green, orange, yellow} = midiLaunchpad.colors
+const {red, green, orange, yellow, off} = midiLaunchpad.colors
 
 const connection = midiLaunchpad.connect()
 
 connection.on('ready', launchpad => {
   launchpad.on('press', button => {
-    console.log(button.x, button.y)
-    button.light(green.high)
+    if(button.getState() !== off) {
+      button.light(off)
+    } else {
+      button.light(green.high)
+    }
   })
 })
