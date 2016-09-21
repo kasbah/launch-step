2c11912eee6b8298f93ac9784ee8f12b1ace0ef2
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-09-21 13:10:39 +0100

    Refactor index.js

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const midi = require('midi')
 const input = new midi.input()
 const output = new midi.output()
-const {red, green, orange, yellow} = require('midi-launchpad').colors
-let port = 0
+const midiLaunchpad = require('midi-launchpad')
+const {red, green, orange, yellow, connect} = midiLaunchpad.colors
 
+let port = 0
 for (let i = 0; i < input.getPortCount(); i++)  {
     if (/Launchpad/.test(input.getPortName(i))) {
         port = i
     }
 }
 
-var midiConnector = require('midi-launchpad').connect(port, false);
+const connection = midiLaunchpad.connect(port)
 
-// wait for the connector to be ready
-midiConnector.on('ready',function(launchpad) {
-  launchpad.on('press', (button) => {
+
+connection.on('ready', launchpad => {
+  launchpad.on('press', button => {
     console.log(button.x, button.y)
     button.light(green.high)
   })
 })
-
