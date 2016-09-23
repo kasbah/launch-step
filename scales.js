6cc38e994346e29127b91b753f8c3ad7f82aaa1a
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-09-23 02:05:09 +0100

    Clean up scales.js

--- scales.js
+++
@@ -1,5 +0,0 @@
-const tonalScales = require('tonal-scales')
-const tonalRanges = require('tonal-rangess')
-function getNotes(scale, root, n) {
-    return tonalRanges.range(tonalScales.scale(scale).join(' '), 0, 127)
-}
