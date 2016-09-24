d8afebaf94699b5921d70d8f6c39f2bca584efc1
Kaspar Emanuel <kaspar.emanuel@gmail.com>
2016-09-24 21:53:46 +0100

    Transpile with babel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 'use strict'
+require("babel-polyfill")
 const midi          = require('midi')
 const midiLaunchpad = require('midi-launchpad')
 const argv          = require('argv')
